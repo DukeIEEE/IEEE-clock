0e15cd3b8614525dc9b472bbdf07ce7dce71cdd6
TomWoo <tomw.82008@gmail.com>
2015-02-17 10:38:33 -0500

    changed clock pins

--- AnalogClock/AnalogClock.ino
+++ AnalogClock/AnalogClock.ino
@@ -37,8 +37,8 @@ int buttonState_h = 0;
 //--------------------------------------------------------------------------------------------------------------------------------
 
 // Set your own pins with these defines !
-#define DS1302_SCLK_PIN   6    // Arduino pin for the Serial Clock
-#define DS1302_IO_PIN     7    // Arduino pin for the Data I/O
+#define DS1302_SCLK_PIN   10    // Arduino pin for the Serial Clock
+#define DS1302_IO_PIN     9    // Arduino pin for the Data I/O
 #define DS1302_CE_PIN     8    // Arduino pin for the Chip Enable
 
 
